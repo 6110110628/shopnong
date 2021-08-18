59a557d2173e29f91148241d3d4ec6ac9a1893c2
6110110628 <6110110628@psu.ac.th>
2021-08-18 13:15:48 +0700

    add template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -67,3 +67,86 @@ export default {
     }
   }
 </page-query>
+
+<style>
+.divide__between{
+  display: flex;
+  justify-content: space-between;
+}
+
+.button{
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    border: 1px solid #5828e8;
+    width: 10rem;
+    text-align: center;
+    justify-content: center;
+    height: 3rem;
+    margin-top: .75rem;
+    margin-bottom: .75rem;
+    border-radius: 5px;
+}
+
+.button:hover{
+  background: #5828e8;
+  color: #f5f5f5;
+}
+.product-content{
+  padding-top: 1.5rem;
+  text-align: center;
+}
+.img{
+  width: 300px;
+  transition-delay: 500ms;
+  object-fit: cover;
+}
+.product-grid{
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -1.5rem;
+  margin-right: -1.5rem;
+  justify-content: space-between;
+}
+.flex-col{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+}
+.link{
+  display: flex;
+  width: 100%;
+  text-decoration: none;
+  height: 100%;
+}
+.product-wrapper{
+  padding: 1.5rem;
+  width: 100%;
+  border-radius: .5rem;
+  position: relative;
+  background-color: #f5f5f5;
+}
+
+.product-name{
+  font-size: 1.125rem;
+  color: #101b42;
+  font-weight: 700;
+  margin-bottom: .5rem;
+}
+.product-name:hover{
+   color: #5828e8;
+}
+.product-price{
+  font-size: .875rem;
+  color: #888;
+  font-weight: bold;
+}
+
+@media(max-width: 760px){
+  .product-grid{
+    justify-content: center;   
+     padding-right: 1rem;
+  }
+}
+</style>
